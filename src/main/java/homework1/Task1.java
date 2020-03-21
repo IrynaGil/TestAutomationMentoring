32dc20a3e392786e3cc626e9a4c6b3e146c63259
IrynaGil <irinagilenenko@gmail.com>
2020-03-21 11:06:47 +0200

    first approach

--- src/main/java/homework1/Task1.java
+++ src/main/java/homework1/Task1.java
@@ -1,4 +1,35 @@
-package HomeWork1;
+package homework1;
 
 public class Task1 {
+    public static void main(String[] args) {
+        seasonWeather1("spring");
+        seasonWeather1("summer");
+        seasonWeather1("autumn");
+        seasonWeather1("winter");
+
+        seasonWeather2("spring");
+        seasonWeather2("summer");
+        seasonWeather2("autumn");
+        seasonWeather2("winter");
+    }
+    public static void seasonWeather1(String season) {
+        if (season == "winter"){
+            System.out.println("It is cold");
+        }
+    }
+
+    public static void seasonWeather2(String season) {
+        if (season == "winter"){
+            System.out.println("It is cold");
+        }
+        else if(season == "spring"){
+            System.out.println("It is not that cold any more");
+        }
+        else if(season == "summer"){
+            System.out.println("It is hot!!!");
+        }
+        else if(season == "autumn"){
+            System.out.println("It is becoming colder...");
+        }
+    }
 }
